238f41a0bf523808ceb694e258a5428325772bed
Mozart D O Nogueira <mozartliimatainen@alu.ufc.br>
2025-06-23 14:32:33 -0300

    correcao - settings
    utilizar tap dos trafos
    neutralizar rede de terceiros

--- bdgd2opendss/model/Transformer.py
+++ bdgd2opendss/model/Transformer.py
@@ -391,10 +391,10 @@ class Transformer:
         self.kvs, self.buses, self.conns, self.kvas, self.taps, kva, MRT = Transformer.adapting_string_variables(self)
 
         if settings.intAdequarTapTrafo: #settings (adequar taps de transformadores)
-            if len(self.taps) < 8:
-                taps = f'taps=[1.0 {self.taps[0:3]}] '
+            if self.windings > 2:
+                taps = f'taps=[1.0 {self.tap} {self.tap}] '
             else:
-                taps = f'taps=[1.0 {self.taps[0:3]} {self.taps[8:11]}] '
+                taps = f'taps=[1.0 {self.tap}] '
         else:
             taps = ""
 
